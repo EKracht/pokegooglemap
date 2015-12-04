55776d29677cf9b5053c075cb593935e00500123
EKracht <ethan1kracht@gmail.com>
2015-12-03 23:57:21 -0800

    working pokegooglemap

--- main.js
+++ main.js
@@ -2,20 +2,26 @@
 
 var map;
 var infowindow;
+var place;
+var rad;
+var center;
+var price;
+var gmarkers = [];
+var pokemon;
+var pokeImage;
 
 function initMap() {
   //get current position
   navigator.geolocation.getCurrentPosition(function(position){
-    var center = {lat: position.coords.latitude, lng: position.coords.longitude};
-    console.log(center);
+    center = {lat: position.coords.latitude, lng: position.coords.longitude};
     map = new google.maps.Map(document.getElementById('map'), {
       center: center,
       zoom: 13
     });
 
     var icon = {
-      url: "https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/128/Map-Marker-Marker-Outside-Azure.png", // url
-      scaledSize: new google.maps.Size(40, 40), // scaled size
+      url: "http://orig09.deviantart.net/66d5/f/2013/266/0/4/satoshi__ash__sprite___kalos_by_binarypeaches-d6nl1n7.gif", // url
+      scaledSize: new google.maps.Size(60, 60), // scaled size
     };
 
     var marker = new google.maps.Marker({
@@ -31,16 +37,64 @@ function initMap() {
 
     infowindow = new google.maps.InfoWindow();
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: center,
-      radius: 8047,
-      types: ['cafe']
-    }, callback);
-
   });
-//
-  // getDirectionsLocation();
+
+  $.ajax("http://pokeapi.co/api/v1/pokedex/1/")
+  .done(function(data){
+    pokemon = data.pokemon;
+  })
+  .fail(function(error){
+    console.log(error);
+  });
+
+  $('#find').on('click', placeMarkers);
+  $('#radius').on('click', filterMarkersByRadius);
+  // $('#price').on('click', filterMarkersByPrice);
+}
+
+function placeMarkers() {
+  removeMarkers();
+  var input = $('#inputPlace').val();
+  place = input;
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: center,
+    radius: 8047,
+    types: [ place ]
+  }, callback);
+}
+
+function filterMarkersByRadius() {
+  removeMarkers();
+  var inputRad = $('#inputRadius').val();
+  rad = inputRad;
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: center,
+    radius: (rad * 1609.34),
+    types: [ place ]
+  }, callback);
+}
+
+// function filterMarkersByPrice() {
+//   removeMarkers();
+//   var inputPrice = $('#inputPrice').val();
+//   price = inputPrice - 1;
+//   console.log('inputPrice', price);
+//   var service = new google.maps.places.PlacesService(map);
+//   service.nearbySearch({
+//     location: center,
+//     radius: (rad * 1609.34 || 8000),
+//     types: [ place ],
+//     minprice: price,
+//     maxprice: price
+//   }, callback);
+// }
+
+function removeMarkers(){
+  for(var i = 0; i < gmarkers.length; i++){
+    gmarkers[i].setMap(null);
+  }
 }
 
 function callback(results, status) {
@@ -52,74 +106,38 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+  var randNum = Math.floor(Math.random() * pokemon.length);
+  $.ajax("http://pokeapi.co/api/v1/pokemon/" + randNum) // request info for one pokemon
+  .done(function(pokemon){ // data received for pokemon
+    $.get("http://pokeapi.co/" + pokemon.sprites[0].resource_uri) // request image info
+    .done(function(data){ // image info received
+      pokeImage = {
+        url: "http://pokeapi.co" + data.image,
+        scaledSize: new google.maps.Size(80, 80), // scaled size
+      }
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        icon: pokeImage
+      });
+      gmarkers.push(marker);
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 
+          'Address: ' + place.vicinity + '<br>' + 'Rating (scale 1-5): ' + 
+          place.rating + '<br>' + 'Price (scale 1-5): ' + place.price_level + 
+          '<br>' + `<img id="theImg" src='${place.icon}'' style="height: 35px" style="width: 35px"/>` + 
+          '<br>' + '</div>');
+        infowindow.open(map, this);
+      });
+    })
+    .fail(function(error){
+      console.log(error);
+    });
+  })
+  .fail(function(error){
+    console.log(error);
   });
 }
 
-
-// 'use strict';
-
-// var map;
-// var center;
-// var service;
-// var infowindow;
-
-// function initialize() {
-//   // navigator.geolocation.getCurrentPosition(function(position){
-//   //   center = {lat: position.coords.latitude, lng: position.coords.longitude};
-//   //   console.log(center);
-//   //   map = new google.maps.Map(document.getElementById('map'), {
-//   //     center: center,
-//   //     zoom: 13
-//   //   });
-//   // })
-//   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
-
-//   map = new google.maps.Map(document.getElementById('map'), {
-//       center: pyrmont,
-//       zoom: 15
-//     });
-
-//   var request = {
-//     location: pyrmont,
-//     radius: 8047,
-//     types: ['cafe'],
-//     query: 'Google Inc.'
-//   }
-//   console.log('past request');
-
-//   service = new google.maps.places.PlacesService(map);
-
-//   console.log(service);
-//   service.nearbySearch(request, callback);
-// }
-
-// function callback(results, status) {
-//   console.log('in callback');
-//   if (status == google.maps.places.PlacesService.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-
-// function createMarker(place) {
-//   var placeLoc = place.geometry.location;
-//   var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//   });
-
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//   });
-// }
